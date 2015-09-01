ddfce183724a80593639940825844a78ddcbfc53
moroz <morozarturv@gmail.com>
2015-09-01 13:02:33 +0300

    main page

--- js/common.js
+++ js/common.js
@@ -1,5 +1,27 @@
 head.ready(function() {
 
-	
+// slider
+(function () {
+	var sl = $('.js-sl');
+	if (sl.length) {
+		sl.slick({
+			rows: 2,
+			slidesPerRow: 4,
+			slidesToShow: 1,
+			arrows: false,
+			dots: true
+		});
+	};
+}());
+
+// slider
+(function () {
+	var sl = $('.js-slider');
+	if (sl.length) {
+		sl.slick({
+			slidesToShow: 5
+		});
+	};
+}());
 	
 });
