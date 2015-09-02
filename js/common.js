ede772af570f7164117bb0c176b750ec10fa3e6c
moroz <arturmorozv@gmail.com>
2015-09-02 13:59:42 +0300

    done some pictures

--- js/common.js
+++ js/common.js
@@ -23,5 +23,52 @@ head.ready(function() {
 		});
 	};
 }());
+
+// videos
+(function () {
+	var sl = $('.js-videos');
+	if (sl.length) {
+		sl.slick({
+			dots: true,
+			arrows: false
+		});
+	};
+}());
+
+// select
+(function () {
+	var select = $('.js-select');
+	if (select.length) {
+		select.each(function () {
+			var _this = $(this);
+			_this.on('change', function () {
+			    var optionSelected = _this.find('option:selected').text();
+			    _this.next().html(optionSelected);
+			    _this.parent().addClass('is-active');
+			});
+		});
+	};
+}());
+
+// search
+(function () {
+	var btn = $('.js-btn-search'),
+		search = $('.js-search');
+	btn.on('click', function () {
+		search.slideToggle();
+		return false;
+	});
+}());
+
+// search
+(function () {
+	var btn = $('.js-category');
+	btn.on('click', function () {
+		var _this = $(this);
+		_this.toggleClass('is-active');
+		_this.next().slideToggle();
+		return false;
+	});
+}());
 	
 });
